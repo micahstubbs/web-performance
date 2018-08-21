80f27b4f5a298127a907432b92978b6be4c99d69
@micahstubbs <micah.stubbs@gmail.com>
2018-08-20 17:07:33 -0700

    show milliseconds on output

--- benchmarks/benchmark.js
+++ benchmarks/benchmark.js
@@ -1,29 +1,33 @@
-const { performance } = require('perf_hooks');
+const { performance } = require('perf_hooks')
 
 // SETUP 🏁
 
-let iterations = 1e7;
+let iterations = 1e7
 
-const a = 1;
-const b = 2;
+const a = 1
+const b = 2
 
-const add = (x, y) => x + y;
+const add = (x, y) => x + y
 
 // 🔚 SETUP
 
-performance.mark('start');
+performance.mark('start')
 
 // EXERCISE 💪
 
 while (iterations--) {
-  add(a, b);
+  add(a, b)
 }
 
 // 🔚 EXERCISE
 
-performance.mark('end');
+performance.mark('end')
 
-performance.measure('My Special Benchmark', 'start', 'end');
+performance.measure('My Special Benchmark', 'start', 'end')
 
-const [ measure ] = performance.getEntriesByName('My Special Benchmark');
-console.log(measure);
+const [measure] = performance.getEntriesByName('My Special Benchmark')
+console.log('PerformanceEntry')
+console.log(`name: ${measure.name}`)
+console.log(`entryType: ${measure.entryType}`)
+console.log(`startTime: ${measure.startTime}`)
+console.log(`duration: ${measure.duration} milliseconds`)
